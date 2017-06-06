4e3c26b56cf5d1d9ff13db6209067cf354e8c88d
Josh Green <josh@softwareverde.com>
2017-06-06 00:23:27 -0400

    Bumped to version 1.0.0.

--- scripts/make-jar.sh
+++ scripts/make-jar.sh
@@ -3,7 +3,7 @@
 rm -rf out/bin 2>/dev/null
 mkdir -p out/bin
 
-version='0.1.0'
+version='1.0.0'
 
 ./gradlew makeJar && cp build/libs/json-${version}.jar out/bin/json-${version}.jar && chmod 770 out/bin/json-${version}.jar
 
